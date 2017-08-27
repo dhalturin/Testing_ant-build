7e85904262d8c4c49e35e0473dbf408e99af099e
Denis Halturin <d.halturin@propellerads.net>
2017-08-28 00:49:09 +0300

    scan directories

--- obf-file.sh
+++ obf-file.sh
@@ -7,14 +7,17 @@ do
     echo "${file} > $(md5sum ${file} | cut -f1 -d' ' | sed 's/ //')" >> $(dirname ${file})/obf-list
 done
 
-find project -type f -name *.pyc | while read file
+find project -type d -name scripts | while read d
 do
-    test -f $(dirname ${file})/obf-list || continue
-    md5=$(md5sum ${file} | cut -f1 -d' ' | sed 's/ //')
-    file_=$(echo ${file} | sed 's/\//\\\//g')
+    find $d -type f -name *.pyc | while read file
+    do
+        test -f $(dirname ${file})/obf-list || continue
+        md5=$(md5sum ${file} | cut -f1 -d' ' | sed 's/ //')
+        file_=$(echo ${file} | sed 's/\//\\\//g')
 
-    sed -is "s/${md5}/${file_}/" $(dirname ${file})/obf-list
+        sed -is "s/${md5}/${file_}/" $(dirname ${file})/obf-list
+    done
 done
 
-find project -name obf-lists -exec cat {} + >> obf-files
+find project -name obf-list -exec cat {} + > obf-files
 find project -name obf-list -delete
